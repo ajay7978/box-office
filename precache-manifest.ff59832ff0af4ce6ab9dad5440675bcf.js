5195c90aefab5e9844e96e036c4a203c735deb72
ajay7978 <ajay.pradhan7978@gmail.com>
2021-07-14 21:36:59 +0530

    Updates

---
+++ precache-manifest.ff59832ff0af4ce6ab9dad5440675bcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbfe2f934d16f331cfced7d6590444c6",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "8072328dc1c083f56293",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "41f6be601d215431c3ff",
+    "url": "/box-office/static/js/2.e6cc4b4e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office/static/js/2.e6cc4b4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8072328dc1c083f56293",
+    "url": "/box-office/static/js/main.86834792.chunk.js"
+  },
+  {
+    "revision": "c850b6c555a3d841251e",
+    "url": "/box-office/static/js/runtime-main.e4c5afaf.js"
+  }
+]);
